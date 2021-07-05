a1be114966f919846517555b4d7c443e6848ab83
Emens7 <magyaristvanlaszlo@gmail.com>
2021-07-05 14:59:00 +0200

    Első 3 feladat

--- task-02/start.js
+++ task-02/start.js
@@ -2,8 +2,9 @@
  * 1. Töltsd be a json állományok kezelését végző modult az azonos mappából és 
  * emeld ki egy változóba az get metódusát.
  */
-
+const { get } = require('./reader');
 /**
+ 
  * 2. A teszteléshez vedd ki a komment blokkból az alábbi kódot.
  */
 /*
@@ -11,3 +12,6 @@
     console.log( await get(9));
 })();
 */
+( async () => {
+    console.log(await get(9));
+}) ();
